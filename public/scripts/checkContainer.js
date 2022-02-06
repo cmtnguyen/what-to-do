631eaa9bf31c06da1f664213f291d7e8e358dfa4
cmtnguyen <66109485+cmtnguyen@users.noreply.github.com>
2022-02-05 16:27:34 -0800

    Delete public directory

--- public/scripts/checkContainer.js
+++
@@ -1,8 +0,0 @@
-const tasks = document.querySelectorAll('.card');
-for (const task of tasks) {
-  task.addEventListener('click', function (event) {
-    if (event.target.tagName === 'INPUT') {
-      document.querySelector(`.check${event.target.id}`).submit();
-    }
-  }, false);
-}
